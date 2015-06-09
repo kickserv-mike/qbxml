8b3a75126314970da145db638283cc147ba2d7ca
Mike Hall <mike.hall@kickserv.com>
2015-06-09 10:44:21 -0500

    Transpec'ed the tests

--- spec/backwards_compatibility.rb
+++ spec/backwards_compatibility.rb
@@ -11,13 +11,13 @@ describe Qbxml do
         old_parse = qb_old.qbxml_to_hash(data, true)
 
         new_parse1 = qb_new.from_qbxml(data)
-        new_parse1.should == { 'qbxml' => old_parse }
+        expect(new_parse1).to eq('qbxml' => old_parse)
 
         # XML is a pain to compare so we can compare the parsed hash resulting
         # from the generated XML instead.
         #
         new_parse2 =  qb_new.from_qbxml(qb_new.to_qbxml(new_parse1))
-        new_parse2.should == { 'qbxml' => old_parse }
+        expect(new_parse2).to eq('qbxml' => old_parse)
       end
     end
   end
